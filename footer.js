76633b8fdae0a7031961d9851c81b50a527c8789
EshanKavinda <eshan98kavinda@gmail.com>
2022-01-22 19:35:04 +0530

    add navigation to all pages

---
+++ footer.js
@@ -0,0 +1,67 @@
+document.write(`
+<div class="footer">
+    <div class="main">
+        <div class="section">
+            <div class="row">
+                <div class="col-md-3 center-column">
+                    <div class="footer-categories">
+                        <h6>CATEGORY LISTS</h6>
+                        <div class="categories-list">
+                            <div class="categoties-items"><a href="">Home</a></div>
+                            <div class="categoties-items"><a href="">Destinations</a></div>
+                            <div class="categoties-items"><a href="">Gallery</a></div>
+
+                        </div>
+                        <div class="categories-list-2">
+                            <div class="categoties-items"><a href="">About Us</a></div>
+                            <div class="categoties-items"><a href="">Contact Us</a></div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 right-column">
+                    <div class="subscribe-footer">
+                        <h6>NEWSLETTER</h6>
+                        <p>Suscribe to our newsletter and get the lastest scoop right to your inbox!</p>
+                        <div class="newsletter-box">
+                            <form action="#" method="post" name="sign-up">
+                                <input type="email" class="input" id="email" name="email"
+                                    placeholder="Your email address" required>
+                                <input type="submit" class="button" id="submit" value="SIGN UP">
+                            </form>
+                        </div>
+                        <p class="cursive">Your email is safe with us, we don't spam.</p>
+                    </div>
+                </div>
+                <div class="col-md-5 right-column">
+                    <div class="footer-logo pull-right"><img src="img/master/logo.png" alt=""></div>
+                    <div class="about-footer  pull-right">
+                        <p style="text-align: right;">At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                            blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
+                            excepturi.</p>
+                    </div>
+                    <div class="social-footer  pull-right">
+                        <h6 style="text-align: right;">Follow Us</h6>
+                        <div class="social-items"><a href="https://www.facebook.com/">
+                                <div class="icon-fa"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+                            </a></div>
+                        <div class="social-items"><a href="#">
+                                <div class="icon-fa"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+                            </a></div>
+                        <div class="social-items"><a href="#">
+                                <div class="icon-fa"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+                            </a></div>
+                        <div class="social-items"><a href="#">
+                                <div class="icon-fa"><i class="fa fa-tripadvisor" aria-hidden="true"></i></div>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+            <hr class="line-footer">
+            <div class="bottom-footer">
+               
+            </div>
+        </div>
+    </div>
+</div> 
+`);
